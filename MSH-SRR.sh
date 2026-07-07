bddb04681ab13baa69929846a827e49bdfb72ffb
Mehrad <mehradpc4545@gmail.com>
2026-07-07 21:42:01 +0330

    Update MSH-SRR.sh
    ! First Msh SRR Update !
    Updated MPKG(Show the list of available plugins)

--- MSH-SRR.sh
+++ MSH-SRR.sh
@@ -241,49 +241,6 @@ while true; do
             echo "Command: msh-refresh"
             ;;
 
-        mshbackuper)
-            echo "Welcome to MshBackuper!"
-            echo "
-______  ___      ______     ________             ______
-___   |/  /_________  /_    ___  __ )_____ _________  /_____  ______________________
-__  /|_/ /__  ___/_  __ \   __  __  |  __  /  ___/_  //_/  / / /__  __ \  _ \_  ___/
-_  /  / / _(__  )_  / / /   _  /_/ // /_/ // /__ _  ,<  / /_/ /__  /_/ /  __/  /
-/_/  /_/  /____/ /_/ /_/    /_____/ \____/ \___/ /_/|_| \__,_/ _  .___/\___//_/
-                                                               /_/
-            "
-
-            read -rp "Which operation? cp/mv: " operation
-            read -rp "file or directory? " type
-
-            if [[ "$type" == "file" ]]; then
-                read -rp "File full path: " filepath
-                read -rp "File name: " filename
-                read -rp "Backup path: " pathup
-                read -rp "Review: $filename from $filepath -> $pathup. Continue? (yes/no): " resume
-
-                if [[ "$resume" == "yes" ]]; then
-                    sudo "$operation" -v "$filepath/$filename" "$pathup"
-                else
-                    echo "Cancelled."
-                fi
-
-            elif [[ "$type" == "directory" ]]; then
-                read -rp "Directory full path: " dirpath
-                read -rp "Directory name: " dirname
-                read -rp "Backup path: " pathup
-                read -rp "Review: $dirname from $dirpath -> $pathup. Continue? (yes/no): " resume
-
-                if [[ "$resume" == "yes" ]]; then
-                    sudo "$operation" -rv "$dirpath/$dirname" "$pathup"
-                else
-                    echo "Cancelled."
-                fi
-
-            else
-                echo "Invalid type!"
-            fi
-            ;;
-
         mpkg)
             echo '
 ███╗   ███╗██████╗ ██╗  ██╗ ██████╗
@@ -295,6 +252,7 @@ _  /  / / _(__  )_  / / /   _  /_/ // /_/ // /__ _  ,<  / /_/ /__  /_/ /  __/  /
             '
             echo "MPKG (Msh Plugin/Package Manager)"
             echo "Usage: mpkg-get <package-name>"
+            echo "mpkg-show"
             ;;
 
         mpkg-get)
@@ -310,6 +268,13 @@ _  /  / / _(__  )_  / / /   _  /_/ // /_/ // /__ _  ,<  / /_/ /__  /_/ /  __/  /
                 curl -s "$URL" -o "$pkgpath/${PKG}.sh"
                 echo "Installed: $PKG"
             fi
+            ;;
+            mpkg-show)
+            echo "Download And Show The Available Plugin.."
+            echo ""
+            wget "https://github.com/MEHRAD111/MshRepo/raw/refs/heads/main/available.plugin"
+            cat available.plugin
+            sudo rm available.plugin
             ;;
 
         *)
