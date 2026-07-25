456f16069ee47c0d072a075dc57a94bfbbe34ab0
Mehrad <mehradpc4545@gmail.com>
2026-07-25 11:14:57 +0330

    Delete MshWebSite directory

--- MshWebSite/other/other.js
+++
@@ -1,6 +0,0 @@
-const img = document.querySelectorAll('img');
-img.forEach(img => {
-    img.addEventListener('mouseover', () => {
-    console.log('MouseOver Image!');
-    });
-});
--- MshWebSite/script/script.js
+++
@@ -1,5 +0,0 @@
-document.querySelectorAll('.buttons, .buttons2').forEach((btn) => {
-  btn.addEventListener('click', () => {
-    btn.innerHTML = '<h3>Loading</h3>';
-  });
-});
