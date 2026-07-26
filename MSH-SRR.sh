3fef8e400dff0bffa6cdb24fb7342989da163b24
MEHRAD111 <mehradpc4545@gmail.com>
2026-07-26 10:53:53 +0330

    SS

--- MSH-SRR.sh
+++ MSH-SRR.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #Start Of Msh
 #Var
-  build=47
+  Build=47
   GREEN='\033[38;5;120m'
   BLUE='\033[38;5;117m'
   YELLOW='\033[38;5;221m'
@@ -165,20 +165,15 @@ while true; do
         codium)
             codium
             ;;
-            docker)
+        docker)
             docker "$args"
             ;;
 
         # --- Msh tools ---
         msh-refresh)
-           BUILD=$(wget -qO- https://raw.githubusercontent.com/MEHRAD111/Msh/refs/heads/Rolling-Release/MSH-SRR.sh \
-           | awk -F': ' '/^Build/ {print $2}')
-           if [ "$BUILD" == "$build" ]; then
-           echo "Your Msh Is Up-To-Date"
-           else
-           echo "A New Version Is Available : $BUILD"
-           echo "Please Update Your Msh"
-           fi
+          echo "Msh Srr GitHub Version:"
+          wget -qO- https://raw.githubusercontent.com/MEHRAD111/Msh/refs/heads/Rolling-Release/MSH-SRR.sh | grep Build   
+          echo "Your Msh Version:$Build"
            ;;
 
         guide)
