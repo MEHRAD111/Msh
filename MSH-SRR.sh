53098a33cecb80558e8a989b62b38749535971d7
MEHRAD111 <mehradpc4545@gmail.com>
2026-08-20 15:16:27 +0330

    s

--- MSH-SRR.sh
+++ MSH-SRR.sh
@@ -205,9 +205,16 @@ while true; do
               ;;
               disk)
               journalctl --disk-usage
+              df -h | less
               ;;
               boots)
               journalctl --list-boots
+              ;;
+              website)
+              firefox web-five-delta-69.vercel.app
+              ;;
+              github)
+              firefox https://github.com/MEHRAD111/Msh
               ;;
               *)
               echo "
@@ -215,10 +222,10 @@ while true; do
                 o888   888o ooooooooo    ooooooooo8 oo oooooo  
                 888     888  888    888 888oooooo8   888   888 
                 888o   o888  888    888 888          888   888 
-                  88ooo88    888ooo88     88oooo888 o888o o888o
+                  88ooo88    888ooo88     88oooo888 o888o o888o 
                             o888                               
                          "
-                    echo "Open | Usage : [log,log2,live-log,kernel-log,reclog,errors,warning,disk,boots]"           
+                    echo "Open | Usage : [log,log2,live-log,kernel-log,reclog,errors,warning,disk,boots,website,github]"           
            esac
             ;;
         mshcenter)
@@ -235,23 +242,11 @@ while true; do
             ;;
 
         mpkg)
-            echo '
-              ███╗   ███╗██████╗ ██╗  ██╗ ██████╗
-              ████╗ ████║██╔══██╗██║ ██╔╝██╔════╝
-              ██╔████╔██║██████╔╝█████╔╝ ██║  ███╗
-              ██║╚██╔╝██║██╔═══╝ ██╔═██╗ ██║   ██║
-              ██║ ╚═╝ ██║██║     ██║  ██╗╚██████╔╝
-              ╚═╝     ╚═╝╚═╝     ╚═╝  ╚═╝ ╚═════╝ V6
-            '
-            echo "MPKG (Msh Plugin/Package Manager)"
-            echo "Usage: mpkg-get, mpkg-show, mpkg-list, mpkg-enable, mpkg-disable, mpkg-run"
-            
-            ;;
-
-        mpkg-get)
-            PKG="$args"
+            case "$args" in
+        get)    
+            PKG="$arg2"
         
-            if [[ -z "$PKG" ]]; then
+            if [[ -z "$arg2" ]]; then
                 echo -e "\033[1;31mUsage: mpkg-get <plugin-name>\033[0m"
             fi
         
@@ -267,47 +262,79 @@ while true; do
             fi
     
             ;;
-
-        mpkg-show)
+        run)
+        if [[ -f "$arg2.sh" ]]; then
+        bash $arg2.sh
+        else
+        echo "Plugin $arg2 Not Found"
+        fi
+        ;;
+        show)
             echo "Download And Show The Available Plugin.."
             echo ""
             wget "https://github.com/MEHRAD111/MshRepo/raw/refs/heads/main/available.plugin"
             cat available.plugin
             sudo rm available.plugin
             ;;
-        mpkg-disable)
-            if [[ -f "$args.sh" ]]; then
-            mv "$args.sh" ".$args.sh"
-            echo "Plugin $args Disabled"
-            elif [ "$args" == "" ]; then
+        disable)
+            if [[ -f "$arg2.sh" ]]; then
+            mv "$arg2.sh" ".$arg2.sh"
+            echo "Plugin $arg2 Disabled"
+            elif [ "$arg2" == "" ]; then
             echo "Usage : mpkg-disable <plugin-name>"
              else
-             echo "Plugin $args Not Found."
+             echo "Plugin $arg2 Not Found."
              fi
             
             ;;
-        mpkg-enable)
-            if [[ -f ".$args.sh" ]]; then
-            mv ".$args.sh" "$args.sh"
-            echo "Plugin $args Enabled"
-            elif [ "$args" == "" ]; then
+        enable)
+            if [[ -f ".$arg2.sh" ]]; then
+            mv ".$arg2.sh" "$arg2.sh"
+            echo "Plugin $arg2 Enabled"
+            elif [ "$arg2" == "" ]; then
             echo "Usage : mpkg-enable <plugin-name>"
              else
-             echo "Plugin $args Not Found."
+             echo "Plugin $arg2 Not Found."
              fi
             ;;
-        mpkg-list)
+        list)
             ls *.sh
             ;;
-            mpkg-run)
-            if [[ -f "$args.sh" ]]; then
-            bash "$args.sh"
-            elif [ "$args" == "" ]; then
+        run)
+            if [[ -f "$arg2.sh" ]]; then
+            bash "$arg2.sh"
+            elif [ "$arg2" == "" ]; then
             echo "Usage : mpkg-run <plugin-name>"
             else
-            echo "Plugin $args Not Found"
+            echo "Plugin $arg2 Not Found"
             fi
             ;;
+        info)
+         if [[ -f "$arg2.sh" ]]; then
+          cat "$arg2.sh" | grep Version
+          cat "$arg2.sh" | grep Branch
+           elif [ "$arg2" == "" ]; then
+          echo "Usage:mpkg info <plugin-name>"
+           else
+          echo "Plugin $arg2 Not Found"
+          fi
+          ;;    
+            *)
+            echo '
+              ███╗   ███╗██████╗ ██╗  ██╗ ██████╗
+              ████╗ ████║██╔══██╗██║ ██╔╝██╔════╝
+              ██╔████╔██║██████╔╝█████╔╝ ██║  ███╗
+              ██║╚██╔╝██║██╔═══╝ ██╔═██╗ ██║   ██║
+              ██║ ╚═╝ ██║██║     ██║  ██╗╚██████╔╝
+              ╚═╝     ╚═╝╚═╝     ╚═╝  ╚═╝ ╚═════╝ V7
+            '
+            echo "MPKG (Msh Plugin/Package Manager)"
+            echo "Usage: get, show, list, enable, disable, run, info"
+            esac
+            
+            ;;
+
+        
         mshfetch)
              echo "
               oooo     oooo  oooooooo8 ooooo ooooo       oooooooo8 oooooooooo  oooooooooo 
@@ -351,7 +378,62 @@ while true; do
              echo "Backup Stopped"
              fi
             ;;
+            lynxctl)
+             git clone https://github.com/MEHRAD111/LynxCTL.git
+            ;;
+            sysmanager)
+             echo "Welcome to System Manager"
+             echo "
+             1.Default Kernel
+             2.Update System
+             3.Disk Usage
+             4.Disk - All
+             5.All info
+             "
+             read -rp ">>>" sysmgr
+             case "$sysmgr" in
+             1)
+             uname -r
+             ;;
+             2)
+             if [[ -f /usr/bin/pacman ]]; then
+             echo "Founded PacMan On Your System"
+             sudo pacman -Syu
+             elif [[ -f /usr/bin/dnf ]]; then 
+             echo "Founded DNF On Your System"
+             sudo dnf update && sudo dnf Upgrade
+             elif [[ -f /usr/bin/apt ]]; then
+             echo "Founded APT On Your System"
+             sudo apt update && sudo apt Upgrade
+             elif [[ -f /usr/bin/zypper ]]; then
+             echo "Founded Zypper On Your System"
+             sudo zypper ref
+             sudo zypper up
+             sudo zypper dup
+             fi
+             ;;
+             3)
+             df -h
+             ;;
+             4)
+             lsblk -f
+             ;;
+             5)
+             uname -a
+             ;;
+             *)
+             echo "
+ oooooooo8                      oooo     oooo                                                                     
+888       oooo   oooo oooooooo8  8888o   888   ooooooo   oo oooooo    ooooooo     oooooooo8 ooooooooo8 oo oooooo  
+ 888oooooo 888   888 888ooooooo  88 888o8 88   ooooo888   888   888   ooooo888  888    88o 888oooooo8   888    888
+        888 888 888          888 88  888  88 888    888   888   888 888    888   888oo888o 888          888       
+o88oooo888    8888   88oooooo88 o88o  8  o88o 88ooo88 8o o888o o888o 88ooo88 8o 888     888  88oooo888 o888o      
+           o8o888                                                                888ooo888                        
+             "
+             echo "SysManager"
+             esac
 
+            ;;
         *)
             echo "Command not found: $command"
             
